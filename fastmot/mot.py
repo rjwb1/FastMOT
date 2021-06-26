89fc1f980d1ee9a9de707f5ced3b5218994f6fa3
thorvald <rbelshaw@sagarobotics.com>
2021-06-26 18:03:12 +0100

    Only Does Inference

--- fastmot/mot.py
+++ fastmot/mot.py
@@ -3,11 +3,11 @@ import logging
 import cv2
 
 from .detector import SSDDetector, YOLODetector, PublicDetector
-from .feature_extractor import FeatureExtractor
-from .tracker import MultiTracker
-from .utils import Profiler
-from .utils.visualization import draw_tracks, draw_detections
-from .utils.visualization import draw_flow_bboxes, draw_background_flow
+#from .feature_extractor import FeatureExtractor
+#from .tracker import MultiTracker
+#from .utils import Profiler
+#from .utils.visualization import draw_tracks, draw_detections
+#from .utils.visualization import draw_flow_bboxes, draw_background_flow
 
 
 LOGGER = logging.getLogger(__name__)
@@ -43,7 +43,7 @@ class MOT:
         self.verbose = verbose
         self.detector_type = DetectorType[config['detector_type']]
         self.detector_frame_skip = config['detector_frame_skip']
-
+        self.detections = []       
         LOGGER.info('Loading detector model...')
         if self.detector_type == DetectorType.SSD:
             self.detector = SSDDetector(self.size, config['ssd_detector'])
@@ -53,19 +53,24 @@ class MOT:
             self.detector = PublicDetector(self.size, self.detector_frame_skip,
                                            config['public_detector'])
 
-        LOGGER.info('Loading feature extractor model...')
-        self.extractor = FeatureExtractor(config['feature_extractor'])
-        self.tracker = MultiTracker(self.size, cap_dt, self.extractor.metric,
-                                    config['multi_tracker'])
+        
+        #LOGGER.info('Loading feature extractor model...')
+        #self.extractor = FeatureExtractor(config['feature_extractor'])
+        #self.tracker = MultiTracker(self.size, cap_dt, self.extractor.metric,
+        #                            config['multi_tracker'])
 
         # reset counter
-        self.frame_count = 0
+        #self.frame_count = 0
 
     @property
-    def visible_tracks(self):
-        # retrieve confirmed and active tracks from the tracker
-        return [track for track in self.tracker.tracks.values()
-                if track.confirmed and track.active]
+    def show_detections(self):
+        return self.detections
+
+    #@property
+    #def visible_tracks(self):
+    #    # retrieve confirmed and active tracks from the tracker
+    #    return [track for track in self.tracker.tracks.values()
+    #            if track.confirmed and track.active]
 
     def initiate(self):
         """
@@ -82,50 +87,49 @@ class MOT:
             The next frame.
         """
         detections = []
-        if self.frame_count == 0:
-            detections = self.detector(frame)
-            self.tracker.initiate(frame, detections)
-        else:
-            if self.frame_count % self.detector_frame_skip == 0:
-                with Profiler('preproc'):
-                    self.detector.detect_async(frame)
+        #if self.frame_count == 0:
+        #    detections = self.detector(frame)
+            #self.tracker.initiate(frame, detections)
+        #else:
+         #   if self.frame_count % self.detector_frame_skip == 0:
+                #with Profiler('preproc'):
+        self.detector.detect_async(frame)
 
-                with Profiler('detect'):
-                    with Profiler('track'):
-                        self.tracker.compute_flow(frame)
-                    detections = self.detector.postprocess()
+                #with Profiler('detect'):
+            #        with Profiler('track'):
+            #            self.tracker.compute_flow(frame)
+        detections = self.detector.postprocess()
+        self.detections = detections
+            #    with Profiler('extract'):
+            #        self.extractor.extract_async(frame, detections)
+            #        with Profiler('track', aggregate=True):
+            #            self.tracker.apply_kalman()
+            #        embeddings = self.extractor.postprocess()
+            #    with Profiler('assoc'):
+            #        self.tracker.update(self.frame_count, detections, embeddings)
+            #else:
+            #    with Profiler('track'):
+            #        self.tracker.track(frame)
 
-                with Profiler('extract'):
-                    self.extractor.extract_async(frame, detections)
-                    with Profiler('track', aggregate=True):
-                        self.tracker.apply_kalman()
-                    embeddings = self.extractor.postprocess()
+        #if self.draw:
+        #    self._draw(frame, detections)
+        #self.frame_count += 1
 
-                with Profiler('assoc'):
-                    self.tracker.update(self.frame_count, detections, embeddings)
-            else:
-                with Profiler('track'):
-                    self.tracker.track(frame)
+    #@staticmethod
+    #def print_timing_info():
+    #    LOGGER.debug('=================Timing Stats=================')
+    #    LOGGER.debug(f"{'track time:':<37}{Profiler.get_avg_millis('track'):>6.3f} ms")
+    #    LOGGER.debug(f"{'preprocess time:':<37}{Profiler.get_avg_millis('preproc'):>6.3f} ms")
+    #    LOGGER.debug(f"{'detect/flow time:':<37}{Profiler.get_avg_millis('detect'):>6.3f} ms")
+    #    LOGGER.debug(f"{'feature extract/kalman filter time:':<37}"
+    #                 f"{Profiler.get_avg_millis('extract'):>6.3f} ms")
+    #    LOGGER.debug(f"{'association time:':<37}{Profiler.get_avg_millis('assoc'):>6.3f} ms")
 
-        if self.draw:
-            self._draw(frame, detections)
-        self.frame_count += 1
-
-    @staticmethod
-    def print_timing_info():
-        LOGGER.debug('=================Timing Stats=================')
-        LOGGER.debug(f"{'track time:':<37}{Profiler.get_avg_millis('track'):>6.3f} ms")
-        LOGGER.debug(f"{'preprocess time:':<37}{Profiler.get_avg_millis('preproc'):>6.3f} ms")
-        LOGGER.debug(f"{'detect/flow time:':<37}{Profiler.get_avg_millis('detect'):>6.3f} ms")
-        LOGGER.debug(f"{'feature extract/kalman filter time:':<37}"
-                     f"{Profiler.get_avg_millis('extract'):>6.3f} ms")
-        LOGGER.debug(f"{'association time:':<37}{Profiler.get_avg_millis('assoc'):>6.3f} ms")
-
-    def _draw(self, frame, detections):
-        draw_tracks(frame, self.visible_tracks, show_flow=self.verbose)
-        if self.verbose:
-            draw_detections(frame, detections)
-            draw_flow_bboxes(frame, self.tracker)
-            draw_background_flow(frame, self.tracker)
-        cv2.putText(frame, f'visible: {len(self.visible_tracks)}', (30, 30),
-                    cv2.FONT_HERSHEY_SIMPLEX, 1, 0, 2, cv2.LINE_AA)
+    #def _draw(self, frame, detections):
+    #    draw_tracks(frame, self.visible_tracks, show_flow=self.verbose)
+    #    if self.verbose:
+    #        draw_detections(frame, detections)
+    #        draw_flow_bboxes(frame, self.tracker)
+    #        draw_background_flow(frame, self.tracker)
+    #    cv2.putText(frame, f'visible: {len(self.visible_tracks)}', (30, 30),
+    #                cv2.FONT_HERSHEY_SIMPLEX, 1, 0, 2, cv2.LINE_AA)
